b5265e5c09bf621c68ce9f1896ef0ccaff315631
jamesdefant <51454386+jamesdefant@users.noreply.github.com>
2021-09-07 01:14:06 -0600

    implement tab switching on setting menu

--- Assets/_Scripts/UI/Button.cs
+++ Assets/_Scripts/UI/Button.cs
@@ -8,7 +8,8 @@ using UnityEngine.EventSystems;
 namespace Com.SoulSki.UI
 {
     [ExecuteInEditMode]
-    public class Button : MonoBehaviour, IPointerClickHandler, IPointerDownHandler, IPointerUpHandler, IPointerEnterHandler, IPointerExitHandler
+    public class Button : MonoBehaviour, IPointerClickHandler, IPointerDownHandler, 
+                          IPointerUpHandler, IPointerEnterHandler, IPointerExitHandler
     {
 
         #region Fields
@@ -17,12 +18,17 @@ namespace Com.SoulSki.UI
         [SerializeField] Color _normalColor = Color.white;
         [SerializeField] Color _highlightedColor = Color.white;
         [SerializeField] Color _pressedColor = Color.grey;
-        [SerializeField] Color _selectedColor = Color.white;
+        [SerializeField] Color _selectedColor = Color.black;
         [SerializeField] Color _disabledColor = Color.grey;
+
+        [Space()]
+        [SerializeField] float _fadeDuration = 0.2f;
 
         [Space()]
         [SerializeField] UnityEvent _onClick;
         [SerializeField] UnityEvent _onMouseEnter;
+
+        bool _isSelected;
 
 
         Image _image;
@@ -35,9 +41,21 @@ namespace Com.SoulSki.UI
             _image.color = _normalColor;
         }
 
+        public void Select()
+        {
+            StartCoroutine(ChangeToColor(_selectedColor));
+            //_image.color = _selectedColor;
+            _isSelected = true;
+        }
+        public void DeSelect()
+        {
+            _image.color = _normalColor;
+            _isSelected = false;
+        }
+
         public void OnPointerClick(PointerEventData eventData)
         {
-            // Callback
+            Select();
             _onClick.Invoke();
         }
         public void OnPointerDown(PointerEventData eventData)
@@ -51,15 +69,34 @@ namespace Com.SoulSki.UI
 
         public void OnPointerEnter(PointerEventData eventData)
         {
-            // Callback
-            _onMouseEnter.Invoke();
+            if (!_isSelected)
+            {
+                _image.color = _highlightedColor;
 
-            _image.color = _highlightedColor;
+                // Callback
+                _onMouseEnter.Invoke();
+            }
         }
 
         public void OnPointerExit(PointerEventData eventData)
         {
-            _image.color = _normalColor;
+            if(!_isSelected)
+                _image.color = _normalColor;
+        }
+
+
+
+        IEnumerator ChangeToColor(Color newColor)
+        {
+            Color oldColor = _image.color;
+            float elapsedTime = 0f;
+
+            while(elapsedTime < _fadeDuration)
+            {
+                elapsedTime += Time.deltaTime;
+                _image.color = Color.Lerp(oldColor, newColor, elapsedTime / _fadeDuration);
+                yield return null;
+            }
         }
     }
 }
--- Assets/_Scripts/UI/TabSystem.cs
+++ Assets/_Scripts/UI/TabSystem.cs
@@ -1,27 +1,59 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
+//using UnityEngine.UI;
 
 namespace Com.SoulSki.UI
 {
     public class TabSystem : MonoBehaviour
     {
-
-        [SerializeField] CustomButton[] _tabs;
+        [SerializeField] Button _selectedTab;
+        [SerializeField] List<Button> _tabs;
+        [Header("Tabs must match windows exactly!")]
         [SerializeField] GameObject[] _windows;
 
 
-        public void Show(int index)
+
+
+        private void Start()
         {
-            if(index < 0 || index > _windows.Length - 1)
+            //if (_tabs.Count > 0)
+            Debug.Log("Select third tab");
+            SelectTab(2);
+            //SelectTab(2);
+        }
+
+
+        public void OnClick_Tab(Button btn)
+        {
+            // Get the index
+            int index = _tabs.IndexOf(btn);
+
+            SelectTab(index);
+        }
+
+        void SelectTab(int index)
+        {
+            if (index < 0 || index > _tabs.Count - 1)
             {
-                Debug.LogErrorFormat("Index ({0}) does not exist in the _windows array.");
+                Debug.LogErrorFormat("Index ({0}) does not exist in the _tabs list.", index);
+                return;
+            }
+            if (index < 0 || index > _windows.Length - 1)
+            {
+                Debug.LogErrorFormat("Index ({0}) does not exist in the _windows array.", index);
                 return;
             }
 
+            Button btn = _tabs[index];
+            if(_selectedTab != null)
+                _selectedTab.DeSelect();
+
+            _selectedTab = btn;
+            btn.Select();
+
             HideAll();
-            _windows[index].gameObject.SetActive(true);
+            _windows[index].SetActive(true);
         }
 
         void HideAll()
